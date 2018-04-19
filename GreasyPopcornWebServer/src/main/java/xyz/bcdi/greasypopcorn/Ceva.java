21431d27e1d957b1535fee9a7754cda0f2193f21
Paul-Cristian <brinzapaul@gmail.com>
2018-04-19 19:53:33 +0300

    Web server now connects to WSS.

--- GreasyPopcornWebServer/src/main/java/xyz/bcdi/greasypopcorn/Ceva.java
+++ GreasyPopcornWebServer/src/main/java/xyz/bcdi/greasypopcorn/Ceva.java
@@ -1,23 +1,35 @@
 package xyz.bcdi.greasypopcorn;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import xyz.bcdi.greasypopcorn.core.Movie;
 
 /**
  * Servlet implementation class Ceva
  */
 public class Ceva extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
+	private Client client;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Ceva() {
         super();
-        // TODO Auto-generated constructor stub
+        client = ClientBuilder.newClient();
     }
 
 	/**
@@ -25,15 +37,30 @@ public class Ceva extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// Create web target and specific path on web target
+		WebTarget webTarget = client.target("http://localhost:8080/GreasyPopcornWebServicesServer/webapi");
+		WebTarget moviesTarget = webTarget.path("movies");
+						
+		// Request
+		Invocation.Builder invocationBuilder = moviesTarget.request(MediaType.APPLICATION_JSON);
+				
+		List<Movie> wssResponse = invocationBuilder.get(new GenericType<List<Movie>>() {});
+		//sysout(invocationBuilder.buildGet().toString();
+		for (Movie m : wssResponse) {
+			//System.out.println(m.getName());
+			response.getWriter().println(m.getMovieID() + " " + m.getName());
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		//doGet(request, response);
+		
 	}
 
 }

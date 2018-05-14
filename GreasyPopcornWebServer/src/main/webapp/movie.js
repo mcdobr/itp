7b39b968906d09b62741d45283a1d7d5b2c0312f
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2018-05-14 21:09:45 +0300

    rendered each user review on a movie

--- GreasyPopcornWebServer/src/main/webapp/movie.js
+++ GreasyPopcornWebServer/src/main/webapp/movie.js
@@ -1,4 +1,4 @@
-var reviewss = null;
+var reviews = [];
 var url = window.location.href;
 var movieID = url.substr(url.length - 1);
 
@@ -8,22 +8,25 @@ function getMovieReviews(ID){
 		url: '/GreasyPopcornWebServicesServer/webapi/reviews/query?movieID='+ID,
 		dataType: 'json'
 	})
-	.done(function(reviews){
-		reviewss = reviews[0].label;
+	.done(function(reviewsArr){
+		for(let i in reviewsArr){
+			reviews.push(`<p>User: ${reviewsArr[i].username}</p><p>Label: ${reviewsArr[i].label}</p><p>Comentariu:${reviewsArr[i].content}</p><p>Rating:${reviewsArr[i].rating}</p><p>Data postare:${reviewsArr[i].reviewTime.dayOfMonth}.${reviewsArr[i].reviewTime.monthValue}.${reviewsArr[i].reviewTime.year}</p>`);
+		}
 	});
 };
 
 getMovieReviews(movieID);
-console.log(reviewss);
+
+console.log(reviews);
 function getMovie(){
-	console.log(reviewss);
+	console.log(reviews);
 	$.ajax({
 		method: 'GET',
 		url: '/GreasyPopcornWebServicesServer/webapi/movies/'+movieID,
 		dataType: 'json'
 	})
 		.done(function(movie){
-			console.log(reviewss);
+			console.log(reviews);
 			const pageRender = `
 			<div class="movie_container">
 				<h1>${movie.name}</h1>
@@ -31,7 +34,7 @@ function getMovie(){
 				
 				<div class="reviewArea">
 					<div class="userReviews_container">
-						${reviewss}
+						${reviews.map(review => (`<div>${review}</div>`)).join('')}
 					</div>
 					<textarea>
 					</textarea>
